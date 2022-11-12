49f093dd0e0fe8d459ab9d621d9653bf64e4a561
Sinisa <sinisha.stojanovic@gmail.com>
2022-11-12 19:51:08 +0100

    Question number and money list item created and styled

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ function App() {
   return (
     <div className="App">
       <div className="main">main window</div>
-      <div className="pyramid">money pyramid</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem active">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
