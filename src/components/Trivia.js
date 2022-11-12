dd9e3df53bf7d2d813f79aeff0009d5934da2024
Sinisa <sinisha.stojanovic@gmail.com>
2022-11-12 22:53:54 +0100

    Correct, wrong animation added to answer style

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -3,10 +3,10 @@ const Trivia = () => {
     <div className="trivia">
       <div className="question">Koji je najbolji youtube kanal</div>
       <div className="answers">
+        <div className="answer correct">Lama</div>
         <div className="answer">Lama</div>
         <div className="answer">Lama</div>
-        <div className="answer">Lama</div>
-        <div className="answer">Lama</div>
+        <div className="answer wrong">Lama</div>
       </div>
     </div>
   );
