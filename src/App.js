dbe9af3744585ddb85838eda83706823dc748e3d
Sinisa <sinisha.stojanovic@gmail.com>
2022-11-12 19:11:27 +0100

    Created two, 2, html tags for main part of the app and part for money pyramid (current question and earned money)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="main">main window</div>
+      <div className="pyramid">money pyramid</div>
+    </div>
+  );
 }
 
 export default App;
